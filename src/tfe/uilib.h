e06f441c3965d26f6d28869fcecf196c17284a18
David Schmidt <david__schmidt@users.sf.net>
2010-04-12 15:57:19 +0000

    Merging in Glenn's TFE structure for now

---
+++ src/tfe/uilib.h
@@ -0,0 +1,91 @@
+/*
+ * uilib.h - Common UI elements for the Windows user interface.
+ *
+ * Written by
+ *  Ettore Perazzoli <ettore@comm2000.it>
+ *  Andreas Boose <viceteam@t-online.de>
+ *  Manfred Spraul <manfreds@colorfullife.com>
+ *
+ * This file is part of VICE, the Versatile Commodore Emulator.
+ * See README for copyright notice.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ *  02111-1307  USA.
+ *
+ */
+
+#ifndef _UILIB_H
+#define _UILIB_H
+
+#include <windows.h>
+
+#define UILIB_SELECTOR_TYPE_FILE_LOAD 0
+#define UILIB_SELECTOR_TYPE_FILE_SAVE 1
+#define UILIB_SELECTOR_TYPE_DIR_EXIST 2
+#define UILIB_SELECTOR_TYPE_DIR_NEW   3
+  
+#define UILIB_SELECTOR_STYLE_DEFAULT       0
+#define UILIB_SELECTOR_STYLE_TAPE          1
+#define UILIB_SELECTOR_STYLE_DISK          2
+#define UILIB_SELECTOR_STYLE_DISK_AND_TAPE 3
+#define UILIB_SELECTOR_STYLE_CART          4
+#define UILIB_SELECTOR_STYLE_SNAPSHOT      5
+#define UILIB_SELECTOR_STYLE_EVENT_START   6
+#define UILIB_SELECTOR_STYLE_EVENT_END     7
+#define UILIB_SELECTOR_STYLES_NUM          8
+
+#define UILIB_FILTER_ALL            (1 << 0)
+#define UILIB_FILTER_PALETTE        (1 << 1)
+#define UILIB_FILTER_SNAPSHOT       (1 << 2)
+#define UILIB_FILTER_DISK           (1 << 3)
+#define UILIB_FILTER_TAPE           (1 << 4)
+#define UILIB_FILTER_ZIP            (1 << 5)
+#define UILIB_FILTER_CRT            (1 << 6)
+#define UILIB_FILTER_BIN            (1 << 7)
+#define UILIB_FILTER_FLIPLIST       (1 << 8)
+#define UILIB_FILTER_ROMSET_FILE    (1 << 9)
+#define UILIB_FILTER_ROMSET_ARCHIVE (1 << 10)
+#define UILIB_FILTER_KEYMAP         (1 << 11)
+#define UILIB_FILTER_LENGTH_MAX     (1 << 12)
+
+
+#define UILIB_DIALOGBOX_MAX 256
+struct uilib_dialogbox_param_s {
+    HWND hwnd;
+    unsigned int idd_dialog;
+    unsigned int idc_dialog;
+    TCHAR string[UILIB_DIALOGBOX_MAX];
+    unsigned int updated;
+};
+typedef struct uilib_dialogbox_param_s uilib_dialogbox_param_t;
+
+
+typedef struct {
+    unsigned int idc;
+    int element_type;
+} uilib_dialog_group;
+
+
+typedef struct {
+    unsigned int idc;
+    unsigned int ids;
+    int element_type;
+} uilib_localize_dialog_param;
+
+
+//extern char *intl_translate_text_new(int en_resource);
+
+#endif
+
---
+++ src/tfe/vice.h
@@ -0,0 +1,104 @@
+/*
+ * vice.h - Main header file for VICE.
+ *
+ * Written by
+ *  Ettore Perazzoli <ettore@comm2000.it>
+ *  Jouko Valta <jopi@stekt.oulu.fi>
+ *  Andreas Boose <viceteam@t-online.de>
+ *
+ * This file is part of VICE, the Versatile Commodore Emulator.
+ * See README for copyright notice.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ *  02111-1307  USA.
+ *
+ */
+
+#ifndef _VICE_H
+#define _VICE_H
+
+/* We use <config.h> instead of "config.h" so that a compilation using
+   -I. -I$srcdir will use ./config.h rather than $srcdir/config.h
+   (which it would do because vice.h was found in $srcdir).  Well,
+   actually automake barfs if the source directory was already
+   configured, so this should not be an issue anymore.  */
+
+/* RGJ Changed it to "config.h" for AppleWin */
+#include "configt.h" /* Automagically created by the `configure' script.  */
+
+/* ------------------------------------------------------------------------- */
+
+/* Portability... */
+
+#if defined(__hpux) || defined(__IBMC__)
+#ifndef _POSIX_SOURCE
+#define _POSIX_SOURCE
+#endif
+#ifndef _INCLUDE_POSIX_SOURCE
+#define _INCLUDE_POSIX_SOURCE
+#endif
+#endif  /* __hpux */
+
+/* FIXME: We currently allow unaligned memory accesses on i386 only, as they
+   allow for some optimizations.  What other architectures could benefit from
+   having this enabled?  (Maybe the PowerPC would?)  */
+#if defined __i386__
+#define ALLOW_UNALIGNED_ACCESS
+#endif
+
+/* RISC OS specific stuff */
+#ifdef __riscos
+#include "ROlib.h"
+#endif
+
+/* ------------------------------------------------------------------------- */
+/* Which OS is using the common keyboard routines?  */
+#if !defined(__riscos) && !defined(__OS2__)
+#define COMMON_KBD
+#endif
+
+/* Which OS is using those ugly scale source coordinates.  */
+#if defined(__MSDOS__)
+#define VIDEO_SCALE_SOURCE
+#endif
+
+/* ------------------------------------------------------------------------- */
+
+/* Internationalization stuff */
+#if defined(ENABLE_NLS) && defined(HAVE_LIBINTL_H)
+#    include <libintl.h>
+#    define _(String) gettext (String)
+#    ifdef gettext_noop
+#        define N_(String) gettext_noop (String)
+#    else
+#        define N_(String) (String)
+#    endif
+#else
+/* Stubs that do something close enough.  */
+#    define _(String) (String)
+#    define N_(String) (String)
+#endif /* ENABLE_NLS */
+
+#ifdef __OS2__
+int yyparse (void);
+#undef __GNUC__
+#endif
+
+#ifdef MINIXVMD
+#undef vfork
+#endif
+
+#endif
+
