65ddbd093cf5c0492b96924c87ec53a4e58a3330
David Schmidt <david__schmidt@users.sf.net>
2010-04-14 17:13:25 +0000

    Move more documentation to web

--- doc/web/buildpost.sh
+++ doc/web/buildpost.sh
@@ -4,4 +4,5 @@ perl -i.orig -p ../../removeMaven.re developing.html
 perl -i.orig -p ../../removeMaven.re ethernet.html
 perl -i.orig -p ../../removeMaven.re history.html
 perl -i.orig -p ../../removeMaven.re index.html
+perl -i.orig -p ../../removeMaven.re operating.html
 cd ../..
