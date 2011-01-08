19fb0131617a82ddf9732256ee5ee476e02a8599
David Schmidt <david__schmidt@users.sf.net>
2011-01-08 14:07:06 +0000

    Add a couple of diagnostic messages to printer support

--- src/printer.cpp
+++ src/printer.cpp
@@ -1,4 +1,25 @@
 /*
+ GSport - an Apple //gs Emulator
+ Copyright (C) 2010 - 2011 by GSport contributors
+ 
+ Based on the KEGS emulator written by and Copyright (C) 2003 Kent Dickey
+
+ This program is free software; you can redistribute it and/or modify it 
+ under the terms of the GNU General Public License as published by the 
+ Free Software Foundation; either version 2 of the License, or (at your 
+ option) any later version.
+
+ This program is distributed in the hope that it will be useful, but 
+ WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
+ or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License 
+ for more details.
+
+ You should have received a copy of the GNU General Public License along 
+ with this program; if not, write to the Free Software Foundation, Inc., 
+ 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+*/
+
+/*
  *  Copyright (C) 2002-2004  The DOSBox Team
  *
  *  This program is free software; you can redistribute it and/or modify
@@ -323,7 +344,7 @@ void CPrinter::updateFont()
 	if (FT_New_Face(FTlib, fontName, 0, &curFont))
 	{
 		
-		printf("Unable to load font %s");
+		printf("Unable to load font %s\n");
 		//LOG_MSG("Unable to load font %s", fontName);
 		curFont = NULL;
 	}
@@ -1821,7 +1842,7 @@ SDL_FreeSurface(image);*/
 			psfile = fopen(fname, "wb");
 			if (!psfile) 
 			{
-				//LOG(LOG_MISC,LOG_ERROR)("PRINTER: Can't open file %s for printer output", fname);
+				printf("PRINTER: Can't open file %s for printer output", fname);
 				return;
 			}
 
